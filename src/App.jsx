ee4cc21b30702da05e88c1deeeb268c7a6d9dda7
h513h <h513h.h513h@gmail.com>
2024-10-04 19:12:12 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AppProvider } from './AppContext';
+import ErrorBoundary from './components/ErrorBoundary';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Completed from './pages/Completed';
@@ -20,16 +21,18 @@ function App() {
   };
 
   return (
-    <AppProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login onLogin={handleLogin} />} />
-          <Route path="/" element={<Home username={username} />} />
-          <Route path="/completed" element={<Completed username={username} />} />
-          <Route path="/processing" element={<Processing username={username} />} />
-        </Routes>
-      </Router>
-    </AppProvider>
+    <ErrorBoundary>
+      <AppProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login onLogin={handleLogin} />} />
+            <Route path="/" element={<Home username={username} />} />
+            <Route path="/completed" element={<Completed username={username} />} />
+            <Route path="/processing" element={<Processing username={username} />} />
+          </Routes>
+        </Router>
+      </AppProvider>
+    </ErrorBoundary>
   );
 }
 
