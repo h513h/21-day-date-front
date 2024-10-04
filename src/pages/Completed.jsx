af34704132af57cf1d1c0d53869fbc08a3fc6caa
h513h <h513h.h513h@gmail.com>
2024-10-04 23:13:20 +0900

    update

--- src/pages/Completed.jsx
+++ src/pages/Completed.jsx
@@ -114,7 +114,7 @@ const Completed = () => {
               {completedTasks.map((task, index) => (
                 <div 
                   key={index} 
-                  className="col-6 col-md-3" 
+                  className="col-4 col-md-3 col-lg-2" 
                   onClick={() => handleTaskClick(task)}
                   role="button"
                 >
